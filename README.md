6b62870dd6a665a2fb1ba707b490004d439509fd
Jatin <jatin.c@greyorange.sg>
2019-05-23 12:18:49 +0530

    Adding Script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # electionResults
 Small Script to show election results on your terminal.
+
+Install requests, bs4, colored before running it.
+Runs on Python 3.
+
+Not detailed, as this script will only be used for a day.
---
+++ electionResults.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+import colored
+from colored import fg, bg, attr
+
+def start():
+    while 1:
+        d = requests.get('https://results.eci.gov.in/pc/en/partywise/index.htm')
+        soup = BeautifulSoup(d.text, 'html.parser')
+        td = soup.findAll('td')
+        useable = td[15:159]
+        data = [('Name', 'Won', 'Leading','Total'),]
+        t = list()
+        for i, item in enumerate(useable):
+            t.append(item.string)
+            if (i+1) % 4 == 0:
+                data.append(tuple(t))
+                t = list()
+        sortdata = Sort_Tuple(data[1:])
+        newList = [data[0]] + sortdata
+        os.system('clear')
+        prprnt(newList[0:15])
+        print('\n\nTotal: 542, Called: ',called(data[1:]))
+        time.sleep(30)
+
+def prprnt(data):
+    for ii, i in enumerate(data):
+        for id,j in enumerate(i):
+            if id == 0:
+                s = str()
+                sl = list(j)
+                spaces = 50 - len(j)
+                for x in range(spaces):
+                    sl.append(' ')
+                for x in sl:
+                    s += x
+                if(ii == 0):
+                    print('%s'% (fg('white')),s,'%s' % (attr('reset'))," ",end="")
+                else:
+                    print('%s'% (fg('blue')),s,'%s' % (attr('reset'))," ",end="")
+            else:
+                s = str()
+                sl = list(j)
+                spaces = 10 - len(j)
+                for x in range(spaces):
+                    sl.append(' ')
+                for x in sl:
+                    s += x
+                if(ii == 0):
+                    print('%s'% (fg('white')),s,'%s' % (attr('reset'))," ",end="")
+                else:
+                    print('%s' %(fg('green')),s,'%s' % (attr('reset'))," ",end="")
+        print('')
+
+def called(data):
+    c = 0
+    for i in data:
+        c+=int(i[1])
+    return c
+    
+def Sort_Tuple(tup):  
+  
+    # reverse = None (Sorts in Ascending order)  
+    # key is set to sort using second element of  
+    # sublist lambda has been used  
+    tup.sort(key = lambda x: int(x[2]), reverse=True)  
+    return tup
